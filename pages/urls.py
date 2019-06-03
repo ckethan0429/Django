ed7ca012ac7157a9cf48ad763eaa926cf6e9c69c
ckethan0429 <ckethan0429@gmail.com>
2019-06-03 16:59:46 +0900

    2019-06-03

--- pages/urls.py
+++ pages/urls.py
@@ -8,4 +8,12 @@ urlpatterns = [
     path('introduce/<str:name>/<int:age>/', views.introduce),
     path('times/<int:num_1>/<int:num_2>/', views.times),
     path('area/<int:radius>/', views.area),
+    path('dtl_example/', views.dtl_example),
+    path('throw/', views.throw),
+    path('catch/', views.catch),
+    path('artii/', views.artii),
+    path('result/', views.result),
+    path('user-new/', views.user_new),
+    path('user-create/',views.user_create),
+    path('static-example',views.static_example),
 ]
